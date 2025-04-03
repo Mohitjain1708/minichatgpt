281f9a8411b4d7fcdc632f685e464efa72265af0
Shyam2208 <2300301530125@ipec.org.in>
2025-04-03 12:53:28 +0530

    Mini chatgpt

---
+++ 2_chatgpt.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "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"
+
+messages = []
+system_msg = input("What type of chatbot would you like to create?\n")
+messages.append({"role": "system", "content": system_msg})
+
+print("Your new assistant is ready!")
+while input != "quit()":
+    message = input()
+    messages.append({"role": "user", "content": message})
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
